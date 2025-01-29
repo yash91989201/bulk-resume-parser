2cc16793825751c58a73cd74a6e916b3e5ff70e4
yash91989201 <yashraj6370@gmail.com>
2025-01-30 01:21:21 +0530

    fix: update rabbitmq config

--- services/word-to-txt/config.py
+++ services/word-to-txt/config.py
@@ -3,17 +3,14 @@ import os
 class CONFIG:
     DOWNLOAD_DIR = "/tmp/word-to-txt"
 
-class RABBITMQ_CONFIG:
-    HOST = os.getenv("RABBITMQ_HOST", "localhost")
-    PORT = int(os.getenv("RABBITMQ_PORT", 5672))
-    USERNAME = os.getenv("RABBITMQ_USERNAME", "guest")
-    PASSWORD = os.getenv("RABBITMQ_PASSWORD", "guest")
-
 class MINIO_CONFIG:
-    ENDPOINT = f"{os.getenv('S3_HOST', 'localhost')}:{os.getenv('S3_PORT',9000)}"
-    ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
-    SECRET_KEY = os.getenv("S3_SECRET_KEY")
+    ENDPOINT = os.getenv("S3_ENDPOINT","localhost:9000") 
+    ACCESS_KEY = os.getenv("S3_ACCESS_KEY","85cBsFp5EOGhDLsPQkur")
+    SECRET_KEY = os.getenv("S3_SECRET_KEY","UaGUBdEj35JxvvFXpiuF2cWApLz7SLZ1pF89Ckza")
     SECURE = os.getenv("S3_USE_SSL", "False").lower() == "true"
+
+class RABBITMQ_CONFIG:
+    URL = os.getenv("RABBITMQ_URL","aqmp://guest:guest@localhost:5672")
 
 class QUEUES:
     EXTRACT_ARCHIVE = "extract_archive_queue"
