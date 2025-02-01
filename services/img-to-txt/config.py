a7ef64f19537b7e98178fd35dc9c78a9235a2a09
yash91989201 <yashraj6370@gmail.com>
2025-02-01 18:53:48 +0530

    update: service config on img to txt

--- services/img-to-txt/config.py
+++ services/img-to-txt/config.py
@@ -6,11 +6,11 @@ load_dotenv()
 class SERVICE_CONFIG:
     DOWNLOAD_DIR = "/tmp/img-to-txt"
     # no of workers spawned to process message from rabbit mq
-    WORKER_COUNT = int(os.getenv("WORKER_COUNT",10))
+    WORKER_COUNT = int(os.getenv("WORKER_COUNT",25))
     # size of the local task queue
-    QUEUE_SIZE = int(os.getenv("QUEUE_SIZE",10))
+    QUEUE_SIZE = int(os.getenv("QUEUE_SIZE",25))
     # no of messages to fetch from rabbitmq queue 
-    CONCURRENCY = int(os.getenv("CONCURRENCY", 10))
+    CONCURRENCY = int(os.getenv("CONCURRENCY", 25))
 
 class RABBITMQ_CONFIG:
     URL = os.getenv("RABBITMQ_URL","aqmp://guest:guest@localhost:5672")
