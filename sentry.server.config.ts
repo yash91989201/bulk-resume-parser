022ebb9fc91927ad57ffe335fc9a5ed1011c76a7
yash91989201 <yashraj6370@gmail.com>
2025-02-13 17:12:05 +0530

    feat: add sentry with default example for testing

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://791b433a4d7a4a23b1d2288cd11699e6@glitchtip-bc04k0kskc4owwgosw8wgog8.bulk-resume-parser.yashraj-jaiswal.site/1",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
