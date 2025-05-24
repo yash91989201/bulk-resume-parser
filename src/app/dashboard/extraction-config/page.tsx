66fa30761aa30ec24298a63f084ef1935a402f00
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-05-24 14:07:42 +0000

    feat: add selector for config version on extraction config page

--- src/app/dashboard/extraction-config/page.tsx
+++ src/app/dashboard/extraction-config/page.tsx
@@ -1,6 +1,14 @@
 import { ExtractionConfigCard } from "@/components/extraction-config-card";
-import { buttonVariants } from "@/ui/button";
 import { caller } from "@/trpc/server";
+import { buttonVariants } from "@/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/ui/dropdown-menu";
+import { Separator } from "@/ui/separator";
+import { ChevronDown, Plus } from "lucide-react";
 import Link from "next/link";
 
 export default async function Page() {
@@ -10,12 +18,24 @@ export default async function Page() {
     <div className="flex flex-col gap-3">
       <div className="flex justify-between">
         <h1 className="text-xl font-bold">Extraction Config</h1>
-        <Link
-          href="/dashboard/extraction-config/new"
-          className={buttonVariants({ variant: "secondary" })}
-        >
-          New Config
-        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger
+            className={buttonVariants({ variant: "secondary" })}
+          >
+            <Plus className="size-4" />
+            New Config
+            <Separator orientation="vertical" />
+            <ChevronDown className="size-4" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem asChild>
+              <Link href="/dashboard/extraction-config/new/v0">V0 Config</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href="/dashboard/extraction-config/new/v1">V1 Config</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
       {extractionConfigs.map((config) => (
         <ExtractionConfigCard
